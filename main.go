4b6ed0ad96df1ed691a148867249c338adce5891
dmendee1 <mendeemendee9@gmail.com/>
2023-06-22 06:33:35 +0800

    Changed

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/dmendee1/simpletest/util"
+	"log"
+
+	"github.com/dmendee1/simpletest/api"
+	db "github.com/dmendee1/simpletest/db/sqlc"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+}
